7e607af536263e0d86938b2218d51b38595c03c2
R.Yoshiyama <yoshiyama.r24@gmail.com>
2022-09-28 12:40:37 +0000

    [Update] 30-hello-cdk/100-cleanup

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -14,13 +14,13 @@ class CdkWorkshopStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        queue = sqs.Queue(
-            self, "CdkWorkshopQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
+        # queue = sqs.Queue(
+        #     self, "CdkWorkshopQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
 
-        topic = sns.Topic(
-            self, "CdkWorkshopTopic"
-        )
+        # topic = sns.Topic(
+        #     self, "CdkWorkshopTopic"
+        # )
 
-        topic.add_subscription(subs.SqsSubscription(queue))
+        # topic.add_subscription(subs.SqsSubscription(queue))
