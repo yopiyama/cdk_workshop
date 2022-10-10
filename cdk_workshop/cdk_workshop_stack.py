b21dfcc716f370c7510d8c7da159acff9ffaf461
Yopiyama <yoshiyama.r24@gmail.com>
2022-10-10 19:49:25 +0900

    [Add] Lambda をデプロイするセクションまで実施

--- cdk_workshop/cdk_workshop_stack.py
+++ cdk_workshop/cdk_workshop_stack.py
@@ -2,9 +2,7 @@ from constructs import Construct
 from aws_cdk import (
     Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
+    aws_lambda,
     aws_sns_subscriptions as subs,
 )
 
@@ -14,13 +12,9 @@ class CdkWorkshopStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # queue = sqs.Queue(
-        #     self, "CdkWorkshopQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-
-        # topic = sns.Topic(
-        #     self, "CdkWorkshopTopic"
-        # )
-
-        # topic.add_subscription(subs.SqsSubscription(queue))
+        hello_lambda = aws_lambda.Function(
+            self, 'HelloHandler',
+            runtime=aws_lambda.Runtime.PYTHON_3_9,
+            code=aws_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
+        )
